87b33c0a4f457caa1f21ad7817010dcdfb653ab0
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-07-22 11:07:49 +0100

    1.0.11

--- KB-00244-Extend-AD-Schema-including-OID-generation.ps1
+++ KB-00244-Extend-AD-Schema-including-OID-generation.ps1
@@ -1,10 +1,17 @@
-#MNSP Version 1.0.7
+$mnspver = "1.0.11"
 
 Clear-Host
 $LogDir = @()
 Write-Host ""
 $LogDir = Read-Host "Path for Transcript log, e.g D:\Temp\ADschemaExtension" 
 $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
+
+function DashedLine {
+Write-host "-----------------------------------------------------------`n"
+}
+
+Write-Host $(Get-Date)
+Write-Host "MNSP Version" $mnspver
 
 Write-Host "Checking for $LogDir"
 
@@ -32,19 +39,29 @@ $GUIDPart += [UInt64]::Parse($GUID.SubString(30,6), "AllowHexSpecifier")
 $OID = [String]::Format("{0}.{1}.{2}.{3}.{4}.{5}.{6}.{7}", $Prefix, $GUIDPart[0], $GUIDPart[1], $GUIDPart[2], $GUIDPart[3], $GUIDPart[4], $GUIDPart[5], $GUIDPart[6])
 Write-Host $OID -ForegroundColor Green
 
+DashedLine
+
 Start-sleep 5
 
 # get AD schema path
 $adSchema = (Get-ADRootDSE).schemaNamingContext
+Write-Host "AD Schema Path:" $adSchema
+DashedLine
 
 # get user schema
 $userSchema = Get-ADObject -SearchBase $adSchema -Filter "Name -eq 'User'"
+Write-Host "user schema" $userSchema
+DashedLine
 
 # set the short name for custom attribute with no spaces
 $attributeName = "mnspAdminNumber"
+Write-Host "AttributeName" $attributeName
+DashedLine
 
 # set the short description for custom attribute
 $attributeDesc = "MNSP Admin Number"
+Write-Host "Attribute Description:" $attributeDesc
+DashedLine
 
 # paste the OID generated earlier
 
@@ -70,14 +87,21 @@ $adAttributes = @{
     isSingleValued = $singleValued;
     searchflags = $indexable
 }
+Write-Host "Custom attributes hashtable:" $adAttributes
+DashedLine
 
 if ($OID -eq "REPLACE_THIS") {
     throw "OID value incorrect - exiting" ## needs better QOS ###
 }
 
 # create the custom attribute in AD schema
-New-ADObject -Name $attributeName -Type attributeSchema -Path $adSchema -OtherAttributes $adAttributes
+Write-Host "PS to execute: New-ADObject -Name $attributeName -Type attributeSchema -Path $adSchema -OtherAttributes $adAttributes -verbose"
+New-ADObject -Name $attributeName -Type attributeSchema -Path $adSchema -OtherAttributes $adAttributes -verbose
+DashedLine
+
 # add the custom attribute to user class
+Write-Host "PS to execute: $userSchema | Set-ADObject -Add @{mayContain = $attributeName} -verbose"
 $userSchema | Set-ADObject -Add @{mayContain = $attributeName} -verbose
+DashedLine
 
 Stop-Transcript
