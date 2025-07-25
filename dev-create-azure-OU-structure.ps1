e223352507bd8e626ac5d895775926e5afeabb5e
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-07-25 08:07:09 +0100

    0.0.1

---
+++ dev-create-azure-OU-structure.ps1
@@ -0,0 +1,26 @@
+$mnspver = "0.0.1"
+
+Clear-Host
+$LogDir = @()
+Write-Host ""
+$LogDir = Read-Host "Path for Transcript log, e.g D:\Temp\ADschemaExtension" 
+$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
+
+function DashedLine {
+Write-host "-----------------------------------------------------------`n"
+}
+
+Write-Host $(Get-Date)
+Write-Host "MNSP Version" $mnspver
+
+Write-Host "Checking for $LogDir"
+
+If(!(test-path -PathType container $LogDir))
+{
+      Write-Warning "$LogDir does not exist exiting script..."
+      throw
+}
+
+Start-Transcript -Path $transcriptlog -Force -NoClobber -Append
+
+Stop-Transcript
