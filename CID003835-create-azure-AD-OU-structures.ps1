f63c780c4811947137daded9548e7521be8c4432
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-07-28 10:00:06 +0100

    0.0.21

--- CID003835-create-azure-AD-OU-structures.ps1
+++ CID003835-create-azure-AD-OU-structures.ps1
@@ -1,11 +1,10 @@
-$mnspver = "0.0.20"
+$mnspver = "0.0.21"
 Clear-Host
 $CID = "" #Change ID e.g: C09826
 $LogDir = @()
 $LogDir = "$env:USERPROFILE\Documents\PS1s\$CID\Logs"
 $tempcsv1 = "$env:USERPROFILE\Documents\PS1s\$CID\Data\tempcsv1.csv"
 Write-Host "MNSP Version:" $mnspver
-Start-Sleep 10
 $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_transcript.log"
 
 clear-host
