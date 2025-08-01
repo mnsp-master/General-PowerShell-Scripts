bff2828a24e34142b5b2cfb34d2b90edaa6bcbdb
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-08-01 11:41:24 +0100

    0.0.22

--- cid003848-mass-create-azure-AD-users.ps1
+++ cid003848-mass-create-azure-AD-users.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.21"
+$mnspver = "0.0.22"
 Clear-Host
 
 $LogDir = @()
@@ -48,8 +48,20 @@ catch {
     exit 1 # Exit the script if CSV import fails
 }
 
-Write-Host "$VerifiedUserData = Get-Content -path $tempcsv4 | convertFrom-csv | where { $_.$FieldMatch01 -like '$FieldString' }"
 Write-Host "School to process:" $FieldMatch01
 Write-Host "Number of records matching selection criteria:" $VerifiedUserData.count
+
+foreach ($user in $VerifiedUserData) {
+    
+    $FirstName = $User.FirstName
+    $LastName = $User.LastName
+    $Email = $User.Email20Chars
+
+    Write-Host "Processing user:"
+    $FirstName $LastName $Email20Chars
+    DashedLine
+
+
+}
 
 Stop-Transcript
