0ba447adcf5b85cfee8e8421a3bb5a18fe2fe8f3
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2025-08-05 15:01:14 +0100

    0.0.26

--- CID003835-create-azure-AD-OU-structures.ps1
+++ CID003835-create-azure-AD-OU-structures.ps1
@@ -1,4 +1,4 @@
-$mnspver = "0.0.25"
+$mnspver = "0.0.26"
 Clear-Host
 $CID = "C03830" #Change ID e.g: C09826
 $LogDir = @()
@@ -35,12 +35,14 @@ foreach ($School in $OUdata ) {
 
         #(un)Comment as required...
         $LeafOUs = @(
+                    #
+        #students
         "$SSN-Year01",
         "$SSN-Year02",
         "$SSN-Year03",
         "$SSN-Year04",
-        "$SSN-Year05",
-        "$SSN-Year06",
+        #"$SSN-Year05",
+        #"$SSN-Year06",
         #"$SSN-Year07",
         #"$SSN-Year08",
         #"$SSN-Year09",
@@ -52,6 +54,14 @@ foreach ($School in $OUdata ) {
         "$SSN-GenericStudentAccounts",
         "$SSN-StudentGroups",
         "$SSN-StudentOffBoarding"
+        #
+        #staff
+        #"$SSN-Delegated",
+        #"$SSN-EmployedStaff",
+        #"$SSN-StaffOffBoarding",
+        #"$SSN-OtherStaff",
+        #"$SSN-Service",
+        #"$SSN-SharedDrives"
         )
 
         $ParentOU = $($school.Parent)
